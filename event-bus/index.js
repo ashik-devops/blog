7fdb2dde953f2f6a5332d1c195c396db64dfc6ad
ashik-devops <mahmud.devops@gmail.com>
2022-12-06 13:43:20 +0100

    event bus in progress

--- event-bus/index.js
+++ event-bus/index.js
@@ -1,23 +1,25 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const axios = require("axios");
+const cors = require("cors");
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
-app.post("/events", (req, res) => {
+app.post("/events", async (req, res) => {
   const event = req.body;
-  axios.post("http://localhost:4000/events", event).catch((err) => {
+  await axios.post("http://localhost:4000/events", event).catch((err) => {
     console.log(err.message);
   });
-  axios.post("http://localhost:4001/events", event).catch((err) => {
+  await axios.post("http://localhost:4001/events", event).catch((err) => {
     console.log(err.message);
   });
-  axios.post("http://localhost:4002/events", event).catch((err) => {
+  await axios.post("http://localhost:4002/events", event).catch((err) => {
     console.log(err.message);
   });
   res.send({ status: "OK" });
 });
-app.listen(40005, () => {
+app.listen(4005, () => {
   console.log("Listening on 4005");
 });
